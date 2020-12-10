ac346f7d6f3791de0be609d5508c188494f83907
Tapajit Dey <tapjdey@gmail.com>
2020-12-10 21:23:19 +0000

    WIP Full Chinese Translation

--- app.R
+++ app.R
@@ -39,6 +39,12 @@ if (!(require(shinyWidgets))){
     install.packages("shinyWidgets", quiet = T)
     require(shinyWidgets)
 }
+if (!(require(showtext))){
+    install.packages("showtext", quiet = T)
+    require(showtext)
+}
+showtext_auto()
+
 
 # file with translations
 i18n <- Translator$new(translation_csvs_path = "data")
@@ -68,7 +74,7 @@ ui <- fluidPage(
     
     h1(i18n$t("InnerSource Project Fitness Assessment Questionnaire")),
     
-    h5(i18n$t('#')),
+    h5(i18n$t('.')),
     
     # All Questions are listed below
     
@@ -82,22 +88,22 @@ ui <- fluidPage(
                                   choices = likert.choices)),
         
         column(3, radioGroupButtons("v1.mvp", 
-                               "Is it a Minimum Viable Product that works and can be experimented on?",
+                                    i18n$t("Is it a Minimum Viable Product that works and can be experimented on?"),
                                choices = list("Yes" = 1, "No" = 0), justified = TRUE, selected = 0,
                                checkIcon = list(yes = icon("ok", lib = "glyphicon")))),
         
         column(3, sliderTextInput("v1.value", 
-                                  label = "The project is valuable to the company",
+                                  label = i18n$t("The project is valuable to the company"),
                                   grid = T, force_edges = TRUE,
                                   choices = likert.choices)),
         
-        column(3, sliderTextInput("v2.use",label = "The project (or some modules) could be widely used by other teams in the company who might depend on it",
+        column(3, sliderTextInput("v2.use",label = i18n$t("The project (or some modules) could be widely used by other teams in the company who might depend on it"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
     ),
     br(),
     fluidRow(
         
-        column(12, checkboxGroupButtons("v1.prob",label = "Is any of the following true for the project? (choose all applicable options)",
+        column(12, checkboxGroupButtons("v1.prob",label = i18n$t("Is any of the following true for the project? (choose all applicable options)"),
                     choices = list("It has to satisfy very strict requirements" = 1, 
                                 "It has confidential IP that should not be exposed" = 2, 
                                 "It is in the critical path of important deliverables" = 1),           justified = TRUE,
@@ -118,90 +124,90 @@ ui <- fluidPage(
     ),
     br(),
     
-    h3("Process compatibility"),
+    h3(i18n$t("Process compatibility")),
     br(),
     
     fluidRow(
-        column(3, sliderTextInput("v3.mod",label =  "The project is modular enough to make changes easy and safe to make",
+        column(3, sliderTextInput("v3.mod",label =   i18n$t("The project is modular enough to make changes easy and safe to make"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v3.doc",label = "All ancillary resources (Adequate Code Documentation, Discussion forum, Bug Tracker, Wiki etc.) are set up for the project",
+        column(3, sliderTextInput("v3.doc",label = i18n$t("All ancillary resources (Adequate Code Documentation, Discussion forum, Bug Tracker, Wiki etc.) are set up for the project"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v3.con",label = "The project has clear and easy-to-find Contribution guidelines, documentation on development environment setup, running tests, etc. making it easy to contribute to",
+        column(3, sliderTextInput("v3.con",label = i18n$t("The project has clear and easy-to-find Contribution guidelines, documentation on development environment setup, running tests, etc. making it easy to contribute to"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
         column(3, radioGroupButtons("v3.vcs", 
-                               "Is all of the code stored in a version control repository that makes branches, pull requests, and integration easy?",
+                                    i18n$t("Is all of the code stored in a version control repository that makes branches, pull requests, and integration easy?"),
                                choices = list("Yes" = 1, "No" = 0), justified = TRUE, selected = 0,
                                checkIcon = list(yes = icon("ok", lib = "glyphicon")))),
     ),
     br(),
     fluidRow(
         column(3, radioGroupButtons("v4.release", 
-                                    "Can releases be made frequently and/ or on a time-based schedule?",
+                                    i18n$t("Can releases be made frequently and/ or on a time-based schedule?"),
                                 choices = list("Yes" = 1, "No" = 0), justified = TRUE, selected = 0,
                                 checkIcon = list(yes = icon("ok", lib = "glyphicon")))),
         
         
-        column(3, sliderTextInput("v4.common",label = "The project uses specialized tools/ framework that outsiders need to learn before they can contribute",
+        column(3, sliderTextInput("v4.common",label = i18n$t("The project uses specialized tools/ framework that outsiders need to learn before they can contribute"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
         column(3, radioGroupButtons("v4.announce", 
-                               "Is there a mechanism for making announcements that anyone in the organization can follow and search? (Examples: Slack, email)",
+                                    i18n$t("Is there a mechanism for making announcements that anyone in the organization can follow and search? (Examples: Slack/ email)"),
                                choices = list("Yes" = 1, "No" = 0), justified = TRUE, selected = 0,
                                checkIcon = list(yes = icon("ok", lib = "glyphicon")))),
         
         column(3, radioGroupButtons("v4.search", 
-                                    "Is there a recorded mechanism for discussion so that all Guest Contributor questions and internal team decisions are searchable by incoming Guest Contributors? (Examples: Slack, online forum)",
+                                    i18n$t("Is there a mechanism to record discussions so that new guest contributors can search for all previous Q-A and internal team decisions? (Examples: Slack/ online forum)"),
                             choices = list("Yes" = 1, "No" = 0), justified = TRUE, selected = 0,
                             checkIcon = list(yes = icon("ok", lib = "glyphicon")))),
     ),
     
     br(),
-    h3("People compatibility"),
+    h3(i18n$t("People compatibility")),
     br(),
     fluidRow(
-        column(3, sliderTextInput("v5.accept",label = "Team Members are ok with accepting code and changes to their code from outsiders",
+        column(3, sliderTextInput("v5.accept",label = i18n$t("Team Members are  willing to accept code from outsiders and make code changes based on their feedback"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v5.see",label = "Team Members are ok with having outsiders see their less-than-perfect code",
+        column(3, sliderTextInput("v5.see",label = i18n$t("Team Members are ok with having outsiders see their less-than-perfect code"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v5.conv",label = "Team Members are ok with having to conduct conversations (that are sometimes difficult) with outsiders about accepting and rejecting their contributions",
+        column(3, sliderTextInput("v5.conv",label = i18n$t("Team Members are ok with having to conduct conversations (that are sometimes difficult) with outsiders about accepting and rejecting their contributions"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v5.mentor",label = "Team Members are ready to mentor and/or learning to mentor contributors from other teams",
+        column(3, sliderTextInput("v5.mentor",label = i18n$t("Team Members are ready to mentor and/or learning to mentor contributors from other teams"),
                                   grid = T, force_edges = TRUE, choices = likert.choices))
     ),
     
     br(),
     fluidRow(
-        column(3, sliderTextInput("v5.work",label = "Team Members are ready to work with outside contributors for fixing any defects on the contributed code",
+        column(3, sliderTextInput("v5.work",label = i18n$t("Team Members are ready to work with external contributors for fixing any defects on the contributed code"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v5.doc",label = "Team Members are willing to create and maintain documentation for outside contributors",
+        column(3, sliderTextInput("v5.doc",label = i18n$t("Team Members are willing to create and maintain documentation for external contributors"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v5.forum",label = "Team Members are willing to participate in forums and answer questions patiently (instead of offline conversations)",
+        column(3, sliderTextInput("v5.forum",label = i18n$t("Team Members are willing to participate in online forums and answer questions patiently (instead of having offline conversations)"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v5.review",label = "Team Members are willing to do code reviews for outside submissions",
+        column(3, sliderTextInput("v5.review",label = i18n$t("Team Members are willing to do code reviews for external contributors' submissions"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
     ),
     
     br(),
     fluidRow(
-        column(3, sliderTextInput("v6.flex",label = "Management is willing to support flexible work requirements and value the time spent on cross-departmental contributions",
+        column(3, sliderTextInput("v6.flex",label = i18n$t("Management is willing to support flexible work requirements and value the time spent on cross-departmental contributions"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v6.merit",label = "Management supports a meritocratic philosophy that appreciates good contributions from all corners",
+        column(3, sliderTextInput("v6.merit",label = i18n$t("Management supports a meritocratic philosophy that appreciates good contributions from all corners"),
                         grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v6.fail",label = "Management understands the difficulties and can accept experimentation, failure, and repositioning to a reasonable extent",
+        column(3, sliderTextInput("v6.fail",label = i18n$t("Management understands the difficulties of a cultural change and can accept experimentation, failure, and repositioning to a reasonable extent"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
         
-        column(3, sliderTextInput("v6.reward",label = "Management is willing to recognize and incentivize the efforts for making InnerSource Successful",
+        column(3, sliderTextInput("v6.reward",label = i18n$t("Management is willing to recognize and incentivize the efforts for making InnerSource Successful"),
                                   grid = T, force_edges = TRUE, choices = likert.choices)),
     ),
     
@@ -210,13 +216,13 @@ ui <- fluidPage(
     br(),
     fluidRow(
         column(2, switchInput(
-            inputId = "alert", label = "Disable Alerts"
+            inputId = "alert", label = i18n$t("Disable Alerts")
         )),
-        column(2, offset = 1, actionBttn("go",label = "Check Results", 
+        column(2, offset = 1, actionBttn("go",label = i18n$t("Check Results"), 
                                  style = "pill", color = "danger",icon = icon("eye"))),
-        column(2, offset = 1, downloadBttn("saveData",label = "Download Scores", 
+        column(2, offset = 1, downloadBttn("saveData",label = i18n$t("Download Scores"), 
                                          style = "jelly", color = "success")),
-        column(2, offset = 1, downloadBttn("savePlot", label = "Download Plots", style = "jelly")),
+        column(2, offset = 1, downloadBttn("savePlot", label = i18n$t("Download Plots"), style = "jelly")),
     ),    
 
     # Show Project Score
@@ -273,10 +279,19 @@ server <- function(input, output, session) {
                   as.numeric(maplist[input$v6.reward])+ as.numeric(maplist[input$v6.fail]))/4
         
         data <- as.data.frame(matrix(c(v1, v2, v3, v4, v5, v6), ncol=6))
+        # If-else for language translation
+        
+        if (input$selected_language == 'en'){
         colnames(data) <- c("Seed Product", "Multiple potential stakeholders", 
                             "Ease of Contribution", "Appropriate tools & practices", 
                             "Team Readiness for InnerSource", 
                             "Management Support" )
+        } else if (input$selected_language == '中文'){
+            colnames(data) <- c("起始产品", "多个潜在利益相关者", 
+                                "易贡献", "适当的工具和做法", 
+                                "团队准备为InnerSource", 
+                                "管理支持" )
+        }
         
         # To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
         data <- rbind(rep(5,6) , rep(0,6) , data)
@@ -315,8 +330,8 @@ server <- function(input, output, session) {
         if (!input$alert){
         sendSweetAlert(
             session = session,
-            title = "Thank you!",
-            text = "See the Result Below",
+            title = i18n$t("Thank you!"),
+            text = i18n$t("See the Result Below"),
             type = "success"
         )}
     })
@@ -324,6 +339,7 @@ server <- function(input, output, session) {
     # generate plots
     
     gen_radarplot <- reactive({
+        showtext_auto()
         par(mar=c(0,0,4,0))
         radarchart( df=getData(), axistype=1, seg = 5,
                     #custom polygon
@@ -331,30 +347,53 @@ server <- function(input, output, session) {
                     #custom the grid
                     cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,5,1), cglwd=0.8,
                     #custom labels
-                    vlcex=1.2, title = "Visual Breakdown of Fitness Score"
+                    vlcex=1.2, title = i18n$t("Visual Breakdown of Fitness Score")
         )
     })
     
+    # If-else for language translation
     gen_lollipopplot <- reactive({
+        showtext_auto()
+        if (input$selected_language == 'en'){
         getData() %>% slice(3) %>% t() %>% as.data.frame() %>% add_rownames() %>% mutate(Category=factor(c(rep("Technology compatibility",2), rep("Process compatibility",2), rep("People compatibility",2)))) %>% arrange(V1) %>% mutate(rowname=factor(rowname, rowname)) %>%
             ggplot( aes(x=rowname, y=V1, color=Category)) +
             geom_segment( aes(x=rowname ,xend=rowname, y=0, yend=V1), color="grey") +
             geom_point(size=5) +
             coord_flip() +
-            theme_ipsum(base_family="sans", base_size = 14)  +
-            ylim(0,5) + ylab("score") + xlab("")+ ggtitle("Visual Breakdown of Fitness Score") +
+            theme_ipsum(base_family="sans", base_size = 14)  + 
+            ylim(0,5) + ylab(i18n$t("score")) + xlab("")+ ggtitle(i18n$t("Visual Breakdown of Fitness Score")) +
             theme( text = element_text(size=14),
                    legend.position="bottom",
                    legend.title = element_blank(),
                    panel.grid.minor.y = element_blank(),
                    panel.grid.major.y = element_blank()
-            ) + guides(fill=guide_legend(nrow=3, byrow=TRUE))
+            ) 
+        } else if (input$selected_language == '中文'){
+            getData() %>% slice(3) %>% t() %>% as.data.frame() %>% add_rownames() %>% mutate(Category=factor(c(rep("技术兼容性",2), rep("工艺兼容性",2), rep("人员兼容性",2)))) %>% arrange(V1) %>% mutate(rowname=factor(rowname, rowname)) %>%
+                ggplot( aes(x=rowname, y=V1, color=Category)) +
+                geom_segment( aes(x=rowname ,xend=rowname, y=0, yend=V1), color="grey") +
+                geom_point(size=5) +
+                coord_flip() +
+                # Font family for showing Chinese Characters
+                theme_ipsum(base_family="wqy-microhei", base_size = 14)  + 
+                ylim(0,5) + ylab(i18n$t("score")) + xlab("")+ ggtitle(i18n$t("Visual Breakdown of Fitness Score")) +
+                theme( text = element_text(size=14),
+                       legend.position="bottom",
+                       legend.title = element_blank(),
+                       panel.grid.minor.y = element_blank(),
+                       panel.grid.major.y = element_blank()
+                ) 
+        }
     })
     
     # render output 
     
     output$score <- renderText({ 
-        paste("The Estimated Fitness Score of this Project is:", getScore(), "; (Range: 0 to 10)")
+        if (input$selected_language == 'en'){
+            paste("The Estimated Fitness Score of this Project is:", getScore(), "; (Range: 0 to 10)")
+        } else if (input$selected_language == '中文'){
+            paste("该项目的估计健身得分为：", getScore(), "; (范围：0至10)")
+        }
     })
     
     output$radarplot <- renderPlot({
@@ -392,7 +431,7 @@ server <- function(input, output, session) {
                         #custom the grid
                         cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,5,1), cglwd=0.8,
                         #custom labels
-                        vlcex=1.2, title = "Visual Breakdown of Fitness Score"
+                        vlcex=1.2, title = i18n$t("Visual Breakdown of Fitness Score")
             )
             print( gen_lollipopplot() )
             dev.off()
